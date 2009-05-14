56558f3e00a5e32e53357fd7f197c223c85619da
Chris Kimpton <kimptoc@chris-kimptons-macbook-pro.local>
2009-05-14 19:25:01 +0100

    more notes

--- MyTwitterDelegate.h
+++ MyTwitterDelegate.h
@@ -11,10 +11,15 @@
 @protocol MyTwitterDelegate 
 
 @optional
+// the set/get config methods are used to interact with your app so that things like the OAuth token can be saved for later re-use.
 	- (void) setConfig:(NSString*) value forKey:(NSString*) key;
 	- (NSString*) configForKey:(NSString*) key;
+	
+// called when a tweet is successfully sent
 	- (void)tweetSucceeded:(NSString *)requestIdentifier;
+// called when a tweet fails
 	- (void)tweetFailed:(NSString *)requestIdentifier withError:(NSError *)error;
+// called with the tweets following a timeline request 
 	- (void)tweetsReceived:(NSArray *)statuses forRequest:(NSString *)identifier;
 
 @end
